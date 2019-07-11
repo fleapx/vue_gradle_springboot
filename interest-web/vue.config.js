c462f3cc2d326e03cc53ad6b4b7869b7d6910401
smallsnail-wh <1147462829@qq.com>
2019-07-11 14:35:47 +0800

    。。。

--- interest-web/vue.config.js
+++ interest-web/vue.config.js
@@ -23,7 +23,7 @@ module.exports = {
         'vue-router': 'VueRouter',
         'vuex':'Vuex',
         'axios': 'axios',
-        'element-ui': 'ELEMENT'
+        'element-ui': 'Element'
     },
     plugins: [
       new webpack.ProvidePlugin({
